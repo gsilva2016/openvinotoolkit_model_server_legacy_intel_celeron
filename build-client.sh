92df8da96bc5d4d3aec88df5f620c80f4e913082
Yg2020sudo <g2020sudo@none.com>
2024-09-19 17:06:29 -0500

    add tf build only

--- build-client.sh
+++ build-client.sh
@@ -4,6 +4,10 @@ if [ "$1" = "from_binary" ]
 then
 	docker build -f Dockerfile.tensorflow-build-binary . -t benchmark_client
 
+elif [ "$1" = "build_tensorflow_only" ]
+then
+	# build tensorflow for legacy celeron
+        docker build -f Dockerfile.build-tensorflow-legacy-celeron . -t build-tensorflow-legacy-celeron
 else
 	# build tensorflow for legacy celeron
 	docker build -f Dockerfile.build-tensorflow-legacy-celeron . -t build-tensorflow-legacy-celeron
